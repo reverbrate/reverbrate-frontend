c27081dd9123358e7d6c8d508878ffcf27a5678a
DEVLevid <levidlima80@gmail.com>
2025-06-17 10:57:42 -0300

    fix: update token retrieval in Home component and add new components structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 
 export default async function Home() {
   const cookieStore = await cookies();
-  const token = cookieStore.get("token");
+  const token = cookieStore.get("acces_token");
 
   if (!token) {
     redirect("/login");

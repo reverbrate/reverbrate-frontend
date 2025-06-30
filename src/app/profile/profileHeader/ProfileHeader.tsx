cb14431e9e0abea8327f1546f301d69df720751d
tzuchaedahy <tzuchaedahy@gmail.com>
2025-06-30 17:05:49 -0300

    implementing profile header

---
+++ src/app/profile/profileHeader/ProfileHeader.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+interface ProfileHeaderProps {
+    name: string;
+    email: string;
+    image: string;
+}
+
+function ProfileHeader({ name, email, image }: ProfileHeaderProps) {
+    return (
+        <div className={styles.profileHeaderContainer}>
+            <Image src={image} alt={"Imagem de Perfil de " + name} width={192} height={192} className={styles.profileHeaderImage}/>
+            <div className={styles.profileHeaderText}>
+                <h3 className={styles.profileHeaderName}>{name}</h3>
+                <p className={styles.profileHeaderEmail}>{email}</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileHeader;

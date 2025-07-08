9daba4552e460f9d727926242ee33d5c69901a13
tzuchaedahy <tzuchaedahy@gmail.com>
2025-07-08 10:44:38 -0300

    add responsivity to profile

--- src/app/profile/profileHeader/profileHeaderSkeleton/ProfileHeaderSkeleton.tsx
+++ src/app/profile/profileHeader/profileHeaderSkeleton/ProfileHeaderSkeleton.tsx
@@ -6,30 +6,16 @@ function ProfileHeaderSkeleton() {
         <div className={styles.profileHeaderContainer}>
             <Skeleton.Input
                 active
-                style={{
-                    width: "128px",
-                    minWidth: "128px",
-                    height: "128px",
-                    backgroundColor: "#1C1825",
-                    borderRadius: "100%",
-                }}
+                className={styles.profileHeaderAvatar}
             />
             <div className={styles.profileHeaderText}>
                 <Skeleton.Input
                     active
-                    style={{
-                        width: "160px",
-                        height: "36px",
-                        backgroundColor: "#1C1825",
-                    }}
+                    className={styles.profileHeaderName}
                 />
                 <Skeleton.Input
                     active
-                    style={{
-                        width: "200px",
-                        height: "20px",
-                        backgroundColor: "#1C1825",
-                    }}
+                    className={styles.profileHeaderBio}
                 />
             </div>
         </div>
